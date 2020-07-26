4c2b896286f1798929b48b214a1efa65fa62c0d8
TheDefede <62037106+TheDefede@users.noreply.github.com>
2020-07-25 23:57:01 -0300

    Handling exception with wrapper lambdas

--- JavaBrains/Lambda/Lambdas/src/ar/com/javabrains/lambdas/StringDemoJava8.java
+++ JavaBrains/Lambda/Lambdas/src/ar/com/javabrains/lambdas/StringDemoJava8.java
@@ -3,6 +3,7 @@ package ar.com.javabrains.lambdas;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.function.Consumer;
 import java.util.function.Predicate;
 
 public class StringDemoJava8 {
@@ -19,22 +20,22 @@ public class StringDemoJava8 {
 		
 
 		
-		printWithCondition(people, p -> true);
+		printWithCondition(people, p -> true, p -> System.out.println(p));
 		
-		printWithCondition(people, p-> p.getLastName().startsWith("D"));
+		printWithCondition(people, p-> p.getLastName().startsWith("D"), p -> System.out.println(p));
 		
 
 		
-		printWithCondition(people,(p)-> p.getFirstName().startsWith("F"));
+		printWithCondition(people,(p)-> p.getFirstName().startsWith("F"), p -> System.out.println(p));
 
 	}
 	
 	
 
-	private static void printWithCondition(List<Person> people, Predicate<Person> condition) {
+	private static void printWithCondition(List<Person> people, Predicate<Person> condition, Consumer<Person> consumer) {
 		for (Person person : people) {
 			if(condition.test(person)) {
-				System.out.println(person);
+				consumer.accept(person);
 			}
 			
 		}
